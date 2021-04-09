ab4410ece6aa1980789e8395c8f06daa4dbeb751
Anton Schwarz <anton@mendoza-schwarz.de>
2021-04-09 12:29:14 +0200

    venv

---
+++ venver.sh
@@ -0,0 +1,8 @@
+REQUIRED_PKG="python3-pip"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
+echo "Checking for $REQUIRED_PKG : $PKG_OK"
+
+if [[ -z $PKG_OK ]]; then
+    echo "$REQUIRED_PKG not found, will install now"
+    sudo apt install $REQUIRED_PKG
+fi
