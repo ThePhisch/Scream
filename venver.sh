520ab15fa1c6589ba903036ac630ef1aa90f272a
Anton Schwarz <anton@mendoza-schwarz.de>
2021-04-09 13:05:38 +0200

    improved setup shell script

--- venver.sh
+++ venver.sh
@@ -1,13 +1,31 @@
 # Prepare environment for Scream
 
 # Install packages
-REQUIRED_PKG="python3-pip"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo "Checking for $REQUIRED_PKG : $PKG_OK"
+# $REQUIRED_PKGS is an array with the required packages
+REQUIRED_PKGS=("python3-pip" "python3-venv")
+for p in "${REQUIRED_PKGS[@]}"; do
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $p | grep "install ok installed")
+    echo "Checking for $p: $PKG_OK"
 
-if [[ -z $PKG_OK ]]; then
-    echo "$REQUIRED_PKG not found, will install now"
-    sudo apt install $REQUIRED_PKG
-fi
+    if [[ -z $PKG_OK ]]; then
+        echo "$p not found, will install now"
+        sudo apt install $p
+    fi
+done
+echo "All packages installed, continue to creation of venv"
 
 # Create Venv
+ENV_NAME="screamenv"
+echo "Create Virtual Environment with the name $ENV_NAME"
+python3 -m venv $ENV_NAME
+
+# Enter Venv, install pip packages
+PYTHON_PKGS=("Flask")
+echo "entering $ENV_NAME, installing pip packages"
+source $ENV_NAME/bin/activate
+for p in "${PYTHON_PKGS[@]}"; do
+    python3 -m pip install $p
+done
+echo "done with $ENV_NAME"
+deactivate
+echo "done"
