bd22efed549109c193e68cd47b6e9c7bca0a5a12
Anton Schwarz <anton@mendoza-schwarz.de>
2021-04-09 12:31:55 +0200

    added comments

--- venver.sh
+++ venver.sh
@@ -1,3 +1,6 @@
+# Prepare environment for Scream
+
+# Install packages
 REQUIRED_PKG="python3-pip"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
 echo "Checking for $REQUIRED_PKG : $PKG_OK"
@@ -6,3 +9,5 @@ if [[ -z $PKG_OK ]]; then
     echo "$REQUIRED_PKG not found, will install now"
     sudo apt install $REQUIRED_PKG
 fi
+
+# Create Venv
