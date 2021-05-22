446c4b37d2018df681b2155fa406c498ccc1b14e
Anton Schwarz <anton@mendoza-schwarz.de>
2021-05-22 13:50:44 +0200

    minor additions (says if login failed). add tests next

--- spp/start/routes.py
+++ spp/start/routes.py
@@ -17,25 +17,25 @@ def index():
 @bp.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("/"))
+        return redirect(url_for("start.index"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            return "login failed"
+            return render_template("login.html", title="Login", form=form, failedAttempt = True) 
         login_user(user)
-        return "login succeeded"
+        return "login succeeded <a href='/'>index</a>"
     return render_template("login.html", title="Login", form=form)
 
 @bp.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for("/"))
+    return redirect(url_for("start.index"))
 
 @bp.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for("/"))
+        return redirect(url_for("start.index"))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data)
